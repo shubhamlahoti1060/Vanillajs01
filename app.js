5a83ce7e43e648620803d015ee7d3ea06cf630e7
shubhamlahoti1060 <shubhamlahoti1060@gmail.com>
2021-03-25 15:02:33 +0530

    added new line

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const texts = ['Websites','illustrations','pancakes'];
+const texts = ['Websites', 'illustrations', 'pancakes'];
 let count = 0;
 let index = 0;
 let currentText = '';
 let letter = '';
 
-(function type(){
-    if(count === texts.length){
-        count = 0;
-    }
-    currentText = texts[count];
-    letter = currentText.slice(0,++index);
+console.log('hii');
 
-    document.querySelector('.typing').textContent = letter;
-    if(letter.length === currentText.length){
-        count++;
-        index = 0;
-    }
-    setTimeout(type,400);
-}());
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('.typing').textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 400);
+})();
